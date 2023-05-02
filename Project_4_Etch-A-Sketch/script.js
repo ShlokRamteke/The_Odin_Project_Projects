2cdf10597ac75ec6931872377e939050db9a40de
krt <52488299+ShlokRamteke@users.noreply.github.com>
2023-05-02 16:39:43 +0530

    adding colour change capability to the grid over mouse hover

--- Project_4_Etch-A-Sketch/script.js
+++ Project_4_Etch-A-Sketch/script.js
@@ -1,5 +1,5 @@
 const DEFAULT_COLOUR= '#333333'
-const DEFAULT_MODE='color'
+const DEFAULT_MODE='colour'
 const DEFAULT_SIZE=16
 
 let currentColour= DEFAULT_COLOUR;
@@ -29,13 +29,60 @@ const sizeSlider=document.getElementById('sizeSlider');
 const grid=document.getElementById('grid');
 
 colourPicker.oninput = (e)=> setCurrentColor(e.target.value);
-colourBtn.onclick = () => setCurrentMode('color');
+colourBtn.onclick = () => setCurrentMode('colour');
 rainbowBtn.onclick = () => setCurrentMode('rainbow');
 erasorBtn.onclick = () => setCurrentMode('erasor');
 clearBtn.onclick = () => reloadGrid();
 sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
 sizeSlider.onchange = (e) => changeSize(e.target.value);
 
+let mouseDown =false
+document.body.onmousedown = () => (mouseDown=true);
+document.body.onmouseup = () => (mouseDown=false);
+
+function changeSize(value){
+    setCurrentSize(value);
+    updateSizeValue(value);
+    reloadGrid();
+}
+
+function updateSizeValue(value) {
+    sizeValue.innerHTML=`${value} x ${value}`;
+}
+
+function reloadGrid() {
+    clearGrid();
+    setupGrid(currentSize);
+}
+
+function clearGrid() {
+    grid.innerHTML='';
+}
+
+function setupGrid(size) {
+    grid.style.gridTemplateColumns= `repreat(${size}, 1fr)`;
+    grid.style.gridTemplateRows= `repreat(${size}, 1fr)`;
+
+    for (let i=0; i <size *size ; i++) {
+        const gridElement=document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mouseover', changeColour);
+        gridElement.addEventListener('mousedown', changeColour);
+        grid.appendChild(gridElement);
+    }
+}
+
+function changeColour(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (currentMode==='rainbow') {
+        const randomR= Math.floor(Math.random() *256)
+        const randomG= Math.floor(Math.random()*256)
+        const randomB = Math.floor(Math.random()*256)
+        e.target.style.backgroundColor=`rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode==='colour') {
+        
+    }
+}
 
 
 
